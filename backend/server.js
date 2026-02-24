819b631f3baef0fd0091e4d25706ea2c0685748f
Irfad C <irfadc500@gmail.com>
2026-02-24 21:17:36 +0530

    Configure backend DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import pool from "./config/db.js";
 
 const app = express();
 
@@ -7,6 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.port || 5000;
+
+const connectDB = async () => {
+  try {
+    await pool.query("SELECT 1");
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Error in connecting database", error.message);
+  }
+};
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port : ${PORT}`);
