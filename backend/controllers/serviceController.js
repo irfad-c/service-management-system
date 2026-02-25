c48768f340d5082fedd92fc9726bb92c295ad1eb
Irfad C <irfadc500@gmail.com>
2026-02-25 16:37:21 +0530

    Fix rows existence check in service update

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -6,12 +6,12 @@ export const createService = async (req, res) => {
   const { title, description, price } = req.body;
 
   if (!title || !price) {
-    return res.status(400).json({ message: "Title and price required" });
+    return res.status(400).json({ message: "title and price required" });
   }
 
   await pool.query(
     "INSERT INTO services (title, description, price, user_id) VALUES (?, ?, ?, ?)",
-    [title, description, price, req.user.id]
+    [title, description, price, req.user.id],
   );
 
   res.status(201).json({ message: "Service created" });
@@ -22,13 +22,13 @@ export const createService = async (req, res) => {
 export const getServices = async (req, res) => {
   const [services] = await pool.query(
     "SELECT * FROM services WHERE user_id = ?",
-    [req.user.id]
+    [req.user.id],
   );
 
   res.json(services);
 };
 
-// Update Service 
+// Update Service
 
 export const updateService = async (req, res) => {
   const { title, description, price } = req.body;
@@ -36,39 +36,38 @@ export const updateService = async (req, res) => {
 
   const [rows] = await pool.query(
     "SELECT * FROM services WHERE id = ? AND user_id = ?",
-    [serviceId, req.user.id]
+    [serviceId, req.user.id],
   );
 
-  if (!rows.length>0) {
-    return res.status(403).json({ message: "Not allowed" });
-  }
-
-  await pool.query(
-    "UPDATE services SET title=?, description=?, price=? WHERE id=?",
-    [title, description, price, serviceId]
-  );
-
-  res.json({ message: "Service updated" });
-};
-
-// Delete Service 
-
-export const deleteService = async (req, res) => {
-  const serviceId = req.params.id;
-
-  const [rows] = await pool.query(
-    "SELECT id FROM services WHERE id = ? AND user_id = ?",
-    [serviceId, req.user.id]
-  );
+  console.log("rows:", rows);
 
   if (!rows.length) {
     return res.status(403).json({ message: "Not allowed" });
   }
 
   await pool.query(
-    "DELETE FROM services WHERE id = ?",
-    [serviceId]
+    "UPDATE services SET title=?, description=?, price=? WHERE id=?",
+    [title, description, price, serviceId],
   );
+
+  res.json({ message: "Service updated" });
+};
+
+// Delete Service
+
+export const deleteService = async (req, res) => {
+  const serviceId = req.params.id;
+
+  const [rows] = await pool.query(
+    "SELECT id FROM services WHERE id = ? AND user_id = ?",
+    [serviceId, req.user.id],
+  );
+
+  if (!rows.length) {
+    return res.status(403).json({ message: "Not allowed" });
+  }
+
+  await pool.query("DELETE FROM services WHERE id = ?", [serviceId]);
 
   res.json({ message: "Service deleted" });
 };
